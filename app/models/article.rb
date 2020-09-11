0d0d24309e93789c48a983a34f349a4b45b49e12
tadeuasarro <tadeutas@gmail.com>
2020-09-11 15:02:49 -0300

    Work on tags, commit to have a break

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
-    has_many :comments
+  has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+  def tag_list=(tags_string)
+  end
 end
