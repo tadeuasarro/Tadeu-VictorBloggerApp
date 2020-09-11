aad7f012fcbfac403675cb0628ef5cd6f2d1acc6
tadeuasarro <tadeutas@gmail.com>
2020-09-11 13:32:54 -0300

    Finish I1, switch roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
 end
